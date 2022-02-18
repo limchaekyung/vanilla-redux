ecf361ba7a32e0e3b7eef77f692d5a0f804da3db
limchaekyung <corud505@naver.com>
2022-02-18 23:45:30 +0900

    vanilla redux button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+import { createStore } from "redux";
 
-let count = 0;
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  // default state initialize
+  // state modify function
+  /* if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  } */
+  switch(action.type){
+    case ADD: return count + 1;
+    case MINUS: return count -1;
+    default: return count;
+  }
+};
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+/* let count = 0;
 
 number.innerText = count;
 
@@ -16,7 +58,9 @@ const handleAdd = () => {
 const handleMinus = () => {
   count = count - 1;
   updateText();
-};
+}; 
+
 add.addEventListener("click", handleAdd);
 
 minus.addEventListener("click", handleMinus);
+*/
